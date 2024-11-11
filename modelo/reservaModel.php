b409cfeb1e51a16d3cfc8b693d4eba55059bfe46
Sergio Mauricio Rivera Lara <mauri090604@hotmail.com>
2024-11-11 17:28:11 -0600

    casi lo ultimo

--- modelo/reservaModel.php
+++ modelo/reservaModel.php
@@ -5,15 +5,17 @@ class ReservaModel{
     public function guardarReserva($pasajeros, $precio, $id_paquete, $comentarios, $nombre, $apellido, $telefono, $correo){
         include_once('conexion.php');
         $cnn=new Conexion();
+        $consula="INSERT INTO Cliente (nombre, apellido, telefono, correo)
+        VALUES ('$nombre', '$apellido', '$telefono', '$correo');";
+        $resultado=$cnn->prepare($consulta);
+        $resultado->execute();
+        
         $consulta="INSERT INTO Reserva (fecha, pasajeros, precio, id_paquete, comentarios)
         VALUES (NOW(), '$pasajeros', '$precio', '$id_paquete', '$comentarios');";
         $resultado=$cnn->prepare($consula);
         $resultado->execute();
         
-        $consula="INSERT INTO Cliente (nombre, apellido, telefono, correo)
-        VALUES ('$nombre', '$apellido', '$telefono', '$correo');";
-        $resultado=$cnn->prepare($consulta);
-        $resultado->execute();
+        
         if($resultado){
             return true;
             
