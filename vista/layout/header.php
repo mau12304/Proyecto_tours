14d79a38c56b2494c2c77d15f1e82698faabbed0
Amin070204 <145484915+Amin070204@users.noreply.github.com>
2024-11-19 14:36:57 -0600

    cambios css

--- vista/layout/header.php
+++ vista/layout/header.php
@@ -17,7 +17,7 @@ $userName = $isAuthenticated && isset($_SESSION['nombre_usuario'])
     <title>Proyecto Turitux</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="Vista/css/style.css">
+    <link rel="stylesheet" href="vista/css/style.css">
     <link rel="stylesheet" href="vista/css/styleSesion.css">
     <link rel="stylesheet" href="vista/css/estyleregistro.css">
     <link rel="stylesheet" href="vista/css/styleconocenos.css">
